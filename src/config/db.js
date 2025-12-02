c4c908a6cda11dbfb3877f7e65152674b11d81ab
priffad <priffad@gmail.com>
2025-12-03 01:28:44 +0800

    coba

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,3 @@
-// src/config/db.js
 const mongoose = require("mongoose");
 
 let isConnected = false;
@@ -6,20 +5,22 @@ let isConnected = false;
 async function connectDB() {
   const uri = process.env.MONGODB_URI;
 
-  console.log("DEBUG DB: MONGODB_URI ada?", !!uri);
+  console.log("DEBUG MONGO: MONGODB_URI ADA?", !!uri);
 
   if (!uri) {
-    console.error("MONGODB_URI belum diset di environment (Vercel / .env)");
+    console.error("MONGODB_URI belum diset di environment");
     return;
   }
 
-  if (isConnected) {
-    return;
-  }
+  if (isConnected) return;
+
+  // log host cluster (tanpa user/pass)
+  const [, afterAt] = uri.split("@");
+  console.log("DEBUG MONGO HOST:", afterAt);
 
   try {
     const conn = await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000, // biar error cepat keluar
+      serverSelectionTimeoutMS: 5000
     });
     isConnected = true;
     console.log("Terhubung ke MongoDB Atlas di host:", conn.connection.host);
